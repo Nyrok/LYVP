fb785fdca1d02335f3ad375cfd433c7b16253205
Nyrok <47859726+Nyrok@users.noreply.github.com>
2023-04-19 19:22:40 +0200

    Async Launch Gif Parts

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from pytube import YouTube, extract
 from moviepy.editor import VideoFileClip
-from time import sleep
+from time import time
+import asyncio
 import pyaudio
 import wave
 import os
 
+
+async def delayed_function(t, func, asc=False, *args):
+    expected = time() + t
+    while True:
+        if time() < expected:
+            continue
+        func(*args) if not asc else asyncio.run(func(*args))
+        break
+
+
 def create_folder(folder_path):
     not os.path.exists(folder_path) and os.mkdir(folder_path)
+
 
 def download(link):
     video_id = extract.video_id(link)
@@ -23,6 +35,7 @@ def download(link):
     print(f"Le téléchargement a été fait avec succès vers {path}")
     divide(video_id)
 
+
 def divide(video_id):
     full_video = VideoFileClip(f"./cache/{video_id}/video/{video_id}.mp4")
     create_folder(f"./cache/{video_id}/audio")
@@ -35,24 +48,27 @@ def divide(video_id):
         clip = full_video.subclip(current_duration - single_duration, current_duration)
         current_duration -= single_duration
         current_video = os.path.realpath(f"./cache/{video_id}/video_parts/{video_id}_{i}.mp4")
-        clip.to_videofile(current_video, codec="libx264", temp_audiofile='temp-audio.m4a', remove_temp=True,
-                          audio_codec='aac')
+        clip.to_videofile(current_video, codec="libx264",
+                          temp_audiofile='temp-audio.m4a', remove_temp=True, audio_codec='aac')
         i += 1
     else:
         clip = full_video.subclip(0, current_duration)
         current_video = os.path.realpath(f"./cache/{video_id}/video_parts/{video_id}_{i}.mp4")
-        clip.to_videofile(current_video, codec="libx264", temp_audiofile='temp-audio.m4a', remove_temp=True,
-                          audio_codec='aac')
-    transform(video_id, i + 1)
+        clip.to_videofile(current_video, codec="libx264",
+                          temp_audiofile='temp-audio.m4a', remove_temp=True, audio_codec='aac')
+    transform(video_id, i)
+
 
 def transform(video_id, n):
     create_folder(f"./cache/{video_id}/gif_parts")
-    for i in range(1, n):
+    for i in range(1, n + 1):
         path = f"./cache/{video_id}/video_parts/{video_id}_{i}.mp4"
         video_part = VideoFileClip(path)
         new_path = f"./cache/{video_id}/gif_parts/{video_id}_{i}.gif"
         video_part.write_gif(new_path, fps=30, program='ffmpeg')
     sound(video_id)
+    asyncio.run(launch(video_id, 1, n))
+
 
 def sound(video_id):
     wf = wave.open(f"./cache/{video_id}/audio/{video_id}.wav", 'rb')
@@ -66,7 +82,18 @@ def sound(video_id):
         stream.write(data)
         data = wf.readframes(1024)
 
+
+async def launch(video_id, i, n):
+    global height, width
+    path = f"./cache/{video_id}/gif_parts/{video_id}_{i}.gif"
+    print(f"Lancement du gif à: {path}")
+    os.system(f"sudo ./led-video-viewer {path} --led-rows={height} --led-cols={width}")
+    if i < n:
+        asyncio.run(delayed_function(60, launch, True, video_id, i + 1, n))
+
+
 if __name__ == '__main__':
-    link = "https://www.youtube.com/watch?v=mVUU_lsTFGw"
-    #input("Entrez un lien YouTube afin de commencer le téléchargement: ")
+    height = int(input("Quelle est la hauteur en led ? "))
+    width = int(input("Quelle est la largeur en led ? "))
+    link = input("Entrez un lien YouTube afin de commencer le téléchargement: ")
     download(link)
